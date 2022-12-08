bc6a7dd32b777adc16ff99a4972486279db4034e
Remenby31 <baptiste.cruvellier31@gmail.com>
2022-12-08 22:45:06 +0100

    Update passePlacementRat.ml

--- tp2/sourceEtu/passePlacementRat.ml
+++ tp2/sourceEtu/passePlacementRat.ml
@@ -2,5 +2,7 @@ open Type
 open Exceptions
 open Ast
 
-type t1 = Ast.AstTds.programme
-type t2 = Ast.AstType.programme
+type t1 = Ast.AstType.programme
+type t2 = Ast.AstPlacement.programme
+
+let analyser (AstType.Programme (nf, np)) =  
